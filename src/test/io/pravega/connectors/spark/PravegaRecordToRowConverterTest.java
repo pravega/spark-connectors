841339193a35fe774fa9efe9628887a667644288
incia94 <incia.anand@dell.com>
2019-05-20 01:06:33 -0700

    Unit Tests

---
+++ src/test/io/pravega/connectors/spark/PravegaRecordToRowConverterTest.java
@@ -0,0 +1,19 @@
+package io.pravega.connectors.spark;
+
+import org.apache.spark.sql.Row;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PravegaRecordToRowConverterTest {
+    PravegaRecordToRowConverter Prtc = new PravegaRecordToRowConverter();
+
+    @Test
+    public void toRow() {
+
+
+
+
+
+    }
+}
