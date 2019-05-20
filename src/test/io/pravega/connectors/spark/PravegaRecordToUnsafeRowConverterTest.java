464b8e0198f9e305931c36a249b3cbbbd04e2b07
incia94 <incia.anand@dell.com>
2019-05-20 03:29:39 -0700

    Unit tests
    Signed-off-by: incia94 <incia.anand@dell.com>

--- src/test/io/pravega/connectors/spark/PravegaRecordToUnsafeRowConverterTest.java
+++ src/test/io/pravega/connectors/spark/PravegaRecordToUnsafeRowConverterTest.java
@@ -1,23 +1,24 @@
 package io.pravega.connectors.spark;
 
-
 import org.apache.spark.sql.catalyst.expressions.codegen.UnsafeRowWriter;
-
 import org.apache.spark.unsafe.types.UTF8String;
-
 import org.junit.Assert;
-
 import org.junit.Test;
 
 
-
+/**
+ * To check if the Pravega Events to UnsafeRow works correctly
+ */
 public class PravegaRecordToUnsafeRowConverterTest {
 
     UnsafeRowWriter writer = new UnsafeRowWriter(2);
+
     private int DEFAULT_CAPACITY = 1 << 16;
 
     @Test
     public void toUnsafeRow() {
+
+
         String  x="Pravega";
         String  y="Spark";
 
@@ -29,9 +30,6 @@ public class PravegaRecordToUnsafeRowConverterTest {
         Assert.assertNotEquals(2,writer.totalSize());
         Assert.assertTrue(writer.totalSize()< DEFAULT_CAPACITY);
         Assert.assertFalse(writer.totalSize()  < 1<<4);
-
-
-
 
     }
 }
--- src/test/io/pravega/connectors/spark/PravegaStreamWriterTest.java
+++ src/test/io/pravega/connectors/spark/PravegaStreamWriterTest.java
@@ -1,34 +1,24 @@
 package io.pravega.connectors.spark;
 
-import io.pravega.client.stream.EventStreamWriter;
-import io.pravega.client.stream.*;
 import io.pravega.client.ClientFactory;
-import io.pravega.client.ClientConfig;
-
+import io.pravega.client.stream.EventStreamWriter;
+import io.pravega.client.stream.Transaction;
+import io.pravega.client.stream.TxnFailedException;
 import org.apache.spark.sql.types.StructType;
-
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.times;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 
 
 
 public class PravegaStreamWriterTest {
 
-
-    /**
-     * @param transactionTimeout  The number of milliseconds for transactions to timeout.
-     * @param schema                   The schema of the input data.
-     */
-
-    private static final ClientConfig CLIENT_CONFIG = ClientConfig.builder().build();
     private static final String SCOPE_NAME = "scope";
     private static final String STREAM_NAME = "stream";
     private static final String ROUTING_KEY = "key";
@@ -62,7 +52,7 @@ public class PravegaStreamWriterTest {
         when(clientFactory.<String>createEventWriter(anyObject(),anyObject(),anyObject())).thenReturn(pravegaWriter);
         when(pravegaWriter.beginTxn()).thenReturn(txn);
 
-        txn.writeEvent(ROUTING_KEY,"HELLO PRAVEGA");
+        txn.writeEvent(ROUTING_KEY, "HELLO PRAVEGA SPARK");
         txn.commit();
 
 
@@ -93,7 +83,7 @@ public class PravegaStreamWriterTest {
 
 
         for (int i = 0; i < 5; i++) {
-            String event = "HELLO PRAVEGA";
+            String event = "HELLO PRAVEGA SPARK";
             txn2.writeEvent("", event);
             txn2.flush();
             Thread.sleep(500);
@@ -120,12 +110,11 @@ public class PravegaStreamWriterTest {
         return writer;
     }
 
-    @SuppressWarnings("unchecked")
     private <T> Transaction<T> mockTransaction() {
         return mock(Transaction.class);
     }
 
-    @SuppressWarnings("unchecked")
+
     private ClientFactory mockClientFactory(){
         return mock(ClientFactory.class);
     }
