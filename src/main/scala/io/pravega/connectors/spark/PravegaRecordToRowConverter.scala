14a259422029d7504a4766cdf314cc3a832be6ec
Incia Anand <38821348+Incia94@users.noreply.github.com>
2019-05-29 00:31:02 +0530

    Update PravegaRecordToRowConverter.scala

--- src/main/scala/io/pravega/connectors/spark/PravegaRecordToRowConverter.scala
+++ src/main/scala/io/pravega/connectors/spark/PravegaRecordToRowConverter.scala
@@ -11,7 +11,6 @@ package io.pravega.connectors.spark
 
 import org.apache.spark.sql.Row
 
-
 /** A simple class for converting Pravega events to UnsafeRow */
 class PravegaRecordToRowConverter {
   def toRow(event: Array[Byte], scope: String, streamName: String, segmentId: Long, offset: Long): Row = {
