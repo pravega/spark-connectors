04f341bdae5b988878bdbc01a840c61ca6d71faf
dellThejas <55413736+dellThejas@users.noreply.github.com>
2020-08-05 13:50:26 -0700

    Issue 2: Set scaling policy

---
+++ src/test/scala/io/pravega/connectors/spark/PravegaSourceProviderTest.scala
@@ -0,0 +1,43 @@
+package io.pravega.connectors.spark
+
+import io.pravega.client.stream.ScalingPolicy.ScalingPolicyBuilder
+import org.scalatest.FunSuite
+
+class PravegaSourceProviderTest extends FunSuite {
+
+  // tests: set scaling as fixed, events, bytes per sec
+
+  test("create fixed rate stream") {
+    val map = Map("default_num_segments" -> "3")
+
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getScaleType.name() == "FIXED_NUM_SEGMENTS")
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getMinNumSegments == 3)
+  }
+
+  test("create KBS stream") {
+    val map = Map("default_num_segments" -> "3"
+      , "default_scale_factor" -> "2"
+      , "default_segment_target_rate_bytes_per_sec" -> "1024")
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getScaleType.name() == "BY_RATE_IN_KBYTES_PER_SEC")
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getMinNumSegments == 3)
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getScaleFactor == 2)
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getTargetRate == 1)
+
+  }
+
+  test("Create events per sec stream") {
+    val map = Map("default_num_segments" -> "3"
+      , "default_scale_factor" -> "2"
+      , "default_segment_target_rate_events_per_sec" -> "20")
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getScaleType.name() == "BY_RATE_IN_EVENTS_PER_SEC")
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getMinNumSegments == 3)
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getScaleFactor == 2)
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getTargetRate == 20)
+
+  }
+
+  test("test empty input to stream config builder") {
+    val map = Map[String, String]()
+    assert(PravegaSourceProvider.buildStreamConfig(map).getScalingPolicy.getScaleType.name() == "FIXED_NUM_SEGMENTS")
+  }
+}
